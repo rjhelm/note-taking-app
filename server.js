427439e7b24f6e3205d1743bbc1553b44a0e90cc
Ryan Helm <rjhelm@github.io>
2021-04-08 16:20:56 -0600

    testing certain paths and features

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const database = require('./db/db');
+// const database = require('./db/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
