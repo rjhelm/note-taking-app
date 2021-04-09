1c667c908b76fc6e7154512aae755e0c1b015c57
Ryan Helm <rjhelm@github.io>
2021-04-08 22:24:48 -0600

    changing code to try and get a cleaner and more streamlined working base

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
 
+// Give each note a unique id in order to delete note at a later time
+const { v4: uuidv4 } = require('uuid');
+
+// Route handling
+module.exports = app => {
+    
+    app.get('/api/notes', (req, res) => {
+        console.log('/n/nExecuting GET notes request');
+
+        let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+        console.log('/nGET request returning notes data: ' + JSON.stringify(data));
+    });
+}
--- server.js
+++ server.js
@@ -6,49 +6,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// ensure that html document loads with appropriate styles and js from the public directory
-app.use(express.static('public'));
+// data parsing setup for Express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-// get notes.html file in public directory
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+// Routes for api and html
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-// get db.json file and return saved notes
-// app.get('/api/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/db/db.json'));
-// });
-
-// route user to main page otherwise
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-app.route('/api/notes')
-.get((req, res) => {
-    let jsonFilePath = path.join(__dirname, "/db/db.json");
-    let newNote = req.body;
-
-    let originalId = 99;
-    for (let i = 0; i < database.length; i++) {
-        let individualNote = database[i];
-
-        if (individualNote.id > originalId) {
-            originalId = individualNote.id;
-        }
-    }
-
-    newNote.id = highestId + 1;
-    database.push(newNote);
-
-    fs.writeFileSync(jsonFilePath, JSON.stringify(database),(err) => {
-        if(err) {
-            return console.log(err);
-        }
-        console.log('Your note was saved!');
-    });
-});
-
-app.listen(PORT, () => console.log('Server listening on port: ' + PORT));
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
